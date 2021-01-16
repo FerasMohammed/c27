d00fe51613b134cbe8844e4ff56115ad61cf4385
Feras Mohammed <ferasmohammed@Ferass-MacBook-Pro.local>
2021-01-16 17:15:54 +1100

    sketch updated

--- sketch.js
+++ sketch.js
@@ -1,10 +1,12 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
 
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
+var chain;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -15,14 +17,13 @@ function setup(){
     engine = Engine.create();
     world = engine.world;
 
-
     ground = new Ground(600,height,1200,20);
     platform = new Ground(150, 305, 300, 170);
 
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
     pig1 = new Pig(810, 350);
-    log1 = new Log(810,260,300, PI/2);
+    log1 = new Log(810,260,300,PI/2);
 
     box3 = new Box(700,240,70,70);
     box4 = new Box(920,240,70,70);
@@ -35,6 +36,9 @@ function setup(){
     log5 = new Log(870,120,150, -PI/7);
 
     bird = new Bird(100,100);
+
+    log6 = new Log(230,180,80,PI/2);
+    chain = new Chain(bird.body,log6.body)
 
 }
 
@@ -61,4 +65,6 @@ function draw(){
 
     bird.display();
     platform.display();
+    log6.display();
+    chain.display();
 }
